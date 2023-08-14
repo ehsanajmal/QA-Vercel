168cc6545a1a517326a622aa00ecf162d800e85f
Muhammad Ehsan <email>
2023-08-14 16:56:47 +0500

    db

--- config/index.js
+++ config/index.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   if (mongoose.connection.readyState !== 1) {
-    await mongoose.connect("mongodb://localhost:27017/node", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://dataset0548:BkcyL14ZcKQKHPD4@cluster0.ydj5zdz.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("DB Connected");
   }
   return mongoose.connection;
